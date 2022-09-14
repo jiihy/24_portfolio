3362d52ed2340b143d21ab071257ebc4416da1c3
jiihy <jiihy@naver.com>
2022-09-14 17:28:18 +0900

    build main_spot_bg

---
+++ myself/blog/_posts/co-working/2022-09-09-merge_cancle.md
@@ -0,0 +1,5 @@
+---
+title: git merge 취소해야될 때 reset vs revert ?!
+categories: [co-working]
+comments: true
+---
--- myself/my_portfolio/src/App.js
+++ myself/my_portfolio/src/App.js
@@ -1,5 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
+import './scss/App.scss';
 
 function App() {
   return (
