6a093c147edc480337629e0f56ec90d3bfbd7205
jiihy <jiihy@naver.com>
2022-09-26 23:42:42 +0900

    section1 영역 추가

--- myself/my_portfolio/src/App.js
+++ myself/my_portfolio/src/App.js
@@ -1,5 +1,13 @@
 import './scss/App.scss';
-import main_profile_img from './img/main_profile_mouse.png'
+// images 
+import day01 from './img/day01.png'
+import day02 from './img/day02.png'
+import day03 from './img/day03.png'
+
+// react gsap
+import React, { useRef, useEffect, useState } from "react";
+// import styled from "styled-components";
+import { gsap } from "gsap"
 
 function App() {
   return (
@@ -7,9 +15,23 @@ function App() {
       <div className="wrap">
         <div className='main_profile_area'>
           <div className='inner'>
+            <h3 className='title_text'> <span className='point'>하루 일과</span> 대공개!</h3>
             <ul className='profile_list'>
-              <li><div className='main_profile_img' src={require('./img/main_profile_mouse.png')}></div></li>
-              <li>내가 개발공부 하는 방법</li>
+              <li className='profile_item'>
+                <img src={day01}></img>
+                <strong>출근</strong>
+                열심히 씨드머니 벌기
+              </li>
+              <li className='profile_item'>
+                <img src={day02}></img>
+                <strong>퇴근</strong>
+                지구력을 위해 운동하기
+              </li>
+              <li className='profile_item'>
+                <img src={day03}></img>
+                <strong>자기 전</strong>
+                개인 스터디 작업
+              </li>
             </ul>
           </div>
         </div>
