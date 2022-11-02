dbcd232758dc6d44ecb3ca376bcf2303d39d748d
jiihy <jiihy@naver.com>
2022-11-03 01:27:21 +0900

    vue 이력서 중간 업로드

---
+++ Port_Vue/resume/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <!-- spot_bg_area -->
+    <section class="wrapper">
+      <div class="blur"></div>
+      <div class="blur"></div>
+      <div class="blur"></div>
+      <h2>sie-log</h2>
+      <p>for the eternal grow</p>    
+    </section>
+    <!-- //spot_bg_area -->
+</template>
+
+<script>
+// spot_bg script 
+const script = document.createElement("script");
+script.src="../src/assets/js/bg_script.js"
+document.body.appendChild(script);
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      
+    }
+  },
+  components: {
+
+  }
+}
+
+</script>
+
+
+
+
+<style lang="scss">
+  @import "./assets/scss/App.scss";
+</style>
+``
